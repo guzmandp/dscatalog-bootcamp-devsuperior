6a68ae653a46c4c27174cf7596301f305ea3073a
Darwin Guzman <guzmandp@gmail.com>
2021-01-19 19:57:24 -0300

    H2 database

---
+++ backend/src/main/java/com/guzmandp/dscatalog/services/CategoryService.java
@@ -0,0 +1,19 @@
+package com.guzmandp.dscatalog.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.guzmandp.dscatalog.entities.Category;
+import com.guzmandp.dscatalog.repositories.CategoryRepository;
+
+@Service
+public class CategoryService {
+	@Autowired
+	private	CategoryRepository repository;
+	
+	public List<Category> findAll(){
+		return repository.findAll();
+	}
+}
