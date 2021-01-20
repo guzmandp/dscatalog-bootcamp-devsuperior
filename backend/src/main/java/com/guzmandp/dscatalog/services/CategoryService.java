c0c08514a4caace422154eba05ee9a112b95023b
Darwin Guzman <guzmandp@gmail.com>
2021-01-20 18:33:14 -0300

    DTO

--- backend/src/main/java/com/guzmandp/dscatalog/services/CategoryService.java
+++ backend/src/main/java/com/guzmandp/dscatalog/services/CategoryService.java
@@ -1,10 +1,12 @@
 package com.guzmandp.dscatalog.services;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.guzmandp.dscatalog.dto.CategoryDTO;
 import com.guzmandp.dscatalog.entities.Category;
 import com.guzmandp.dscatalog.repositories.CategoryRepository;
 
@@ -13,7 +15,8 @@ public class CategoryService {
 	@Autowired
 	private	CategoryRepository repository;
 	
-	public List<Category> findAll(){
-		return repository.findAll();
+	public List<CategoryDTO> findAll(){
+		List<Category> list = repository.findAll();
+		return list.stream().map(x -> new CategoryDTO(x)).collect(Collectors.toList());
 	}
 }
