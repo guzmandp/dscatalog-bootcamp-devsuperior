8a0aecbb28e329072e2ee91ef8b1473a2ca00e11
Darwin Guzman <guzmandp@gmail.com>
2021-01-22 18:57:00 -0300

    Update category

--- backend/src/main/java/com/guzmandp/dscatalog/resources/exceptions/ResourceExceptionHandler.java
+++ backend/src/main/java/com/guzmandp/dscatalog/resources/exceptions/ResourceExceptionHandler.java
@@ -9,12 +9,12 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-import com.guzmandp.dscatalog.services.exceptions.EntityNotFoundException;
+import com.guzmandp.dscatalog.services.exceptions.ResourceNotFoundException;
 
 @RestControllerAdvice
 public class ResourceExceptionHandler {
-	@ExceptionHandler(EntityNotFoundException.class)
-	public ResponseEntity<StandardError> entityNotFound(EntityNotFoundException e, HttpServletRequest request){
+	@ExceptionHandler(ResourceNotFoundException.class)
+	public ResponseEntity<StandardError> entityNotFound(ResourceNotFoundException e, HttpServletRequest request){
 	StandardError err = new StandardError();
 	err.setTimestamp(Instant.now());
 	err.setStatus(HttpStatus.NOT_FOUND.value());
