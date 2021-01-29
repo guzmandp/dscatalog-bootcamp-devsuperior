607df51bd10b17ff35dd9126c628c121de9a5613
Darwin Guzman <guzmandp@gmail.com>
2021-01-28 21:19:32 -0300

    Add front-web

---
+++ front-web/src/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import 'bootstrap/dist/css/bootstrap.css';
+import App from './App';
+// Put any other imports below so that CSS from your
+// components takes precedence over default styles.
+
+
+
+
+
+ReactDOM.render(
+ <App/>  ,
+  document.getElementById('root')
+);
