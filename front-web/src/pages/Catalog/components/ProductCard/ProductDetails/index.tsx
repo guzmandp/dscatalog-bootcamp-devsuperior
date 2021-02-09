1fed07b2e1002ae819dd0aac5ae393018c22da7a
Darwin Guzman <guzmandp@gmail.com>
2021-02-08 22:02:39 -0300

    Finish catalog loaders

--- front-web/src/pages/Catalog/components/ProductCard/ProductDetails/index.tsx
+++ front-web/src/pages/Catalog/components/ProductCard/ProductDetails/index.tsx
@@ -1,9 +1,11 @@
 import React, { useEffect, useState } from 'react';
 import { Link, useParams } from 'react-router-dom';
-import {ReactComponent as ArrowIcon} from '../../../../../core/assets/images/arrow.svg';
+import { ReactComponent as ArrowIcon } from '../../../../../core/assets/images/arrow.svg';
 import ProductPrice from '../../../../../core/components/ProductPrice';
 import { Product } from '../../../../../core/types/Product';
 import { makeRequest } from '../../../../../core/utils/request';
+import ProductDescriptionLoader from '../../Loaders/ProductDescriptionLoader';
+import ProductInfoLoader from '../../Loaders/ProductInfoLoader';
 import './styles.scss'
 
 type ParamsType = {
@@ -12,39 +14,53 @@ type ParamsType = {
 
 const ProductDetails = () => {
     const { productId } = useParams<ParamsType>();
-    const [product, setProduct] = useState<Product>(); 
+    const [product, setProduct] = useState<Product>();
+    const [isLoading, setIsLoanding] = useState(false);
 
-    useEffect(()=> {
-        makeRequest({url: `/products/${productId}`})
-        .then(response => setProduct(response.data));
-        }, [productId]);
+    useEffect(() => {
+        setIsLoanding(true);
+        makeRequest({ url: `/products/${productId}` })
+            .then(response => setProduct(response.data))
+            .finally(() => setIsLoanding(false));
+    }, [productId]);
 
     return (
-        <div className= "product-details-container">
-            <div  className="card-base border-radius-20 product-details">
-            <Link to="/products" className="product-details-goback">
-            <ArrowIcon className ="icon-goback" />
-            <h1 className="text-goback">voltar</h1>
-            </Link>
-            <div className="row">
-                <div className = "col-6 pr-5">
-                    <div className="product-details-card text-center">
-                        <img src={product?.imgUrl} alt={product?.name} className="product-details-image" />
+        <div className="product-details-container">
+            <div className="card-base border-radius-20 product-details">
+                <Link to="/products" className="product-details-goback">
+                    <ArrowIcon className="icon-goback" />
+                    <h1 className="text-goback">voltar</h1>
+                </Link>
+                <div className="row">
+                    <div className="col-6 pr-5">
+
+                        {isLoading ? <ProductInfoLoader /> : (
+                            <>
+                                <div className="product-details-card text-center">
+                                    <img src={product?.imgUrl} alt={product?.name} className="product-details-image" />
+                                </div>
+                                <h2 className="product-datails-name">
+                                    {product?.name}
+                                </h2>
+                                {product?.price && <ProductPrice price={product?.price} />}
+                            </>
+                        )}
+
                     </div>
-                    <h2 className="product-datails-name">
-                    {product?.name}
-                    </h2>
-                    {product?.price && <ProductPrice price={product?.price} /> }
+                    <div className="col-6 product-details-card">
+                        {isLoading ? <ProductDescriptionLoader /> : (
+                            <>
+                                <h2 className="product-description-title">
+                                    Descriçao de Produto
+                                </h2>
+                                <p className="product-description-text">
+                                    {product?.description}
+                                </p>
+                            </>
+
+                        )}
+                    </div>
                 </div>
-                <div className = "col-6 product-details-card">
-                    <h2 className="product-description-title">
-                        Descriçao de Produto
-                    </h2>
-                    <p className="product-description-text">
-                        {product?.description}
-                    </p>
-                </div>
-            </div>
             </div>
         </div>
     );
--- front-web/src/pages/Catalog/index.tsx
+++ front-web/src/pages/Catalog/index.tsx
@@ -3,21 +3,29 @@ import { Link } from 'react-router-dom';
 import { ProductsResponse } from '../../core/types/Product';
 import { makeRequest } from '../../core/utils/request';
 import ProductCard from './components/ProductCard';
+import ProductCardLoader from './components/Loaders/ProductCardLoader';
 import './styles.scss';
 
 const Catalog = () => {
 
     const [productsResponse, setProductsResponse] = useState<ProductsResponse>();
-    
-    console.log(productsResponse);
+    const [isLoading, setIsLoanding] = useState(false);
+    //console.log(productsResponse);
   
     useEffect(()=> {
         const params = {
             page: 0,
             linesPerPage: 12
         }
+
+        
+        setIsLoanding(true);
         makeRequest({ url:'/products', params})
-        .then(response => setProductsResponse(response.data));
+        .then(response => setProductsResponse(response.data))
+        .finally(() =>{
+        
+        setIsLoanding(false);
+        })
     }, []);
     return (
         <div className="catalog-container">
@@ -25,11 +33,14 @@ const Catalog = () => {
                 Catálogo de produtos
             </h1>
             <div className="catalog-products">
-                {productsResponse?.content.map(product => (
-                    <Link to={`/products/${product.id}`} key={product.id}>
-                        <ProductCard product={product} />
-                    </Link>
-                ))}
+                {isLoading ? <ProductCardLoader/> : (
+                productsResponse?.content.map(product => (
+                <Link to={`/products/${product.id}`} key={product.id}>
+                <ProductCard product={product} />
+                </Link>
+            ))
+                )}
+            
             </div>
         </div>
         
