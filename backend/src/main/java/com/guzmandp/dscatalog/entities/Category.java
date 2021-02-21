5dd5729715841ca144a8b194d17ab0dc503d96f0
Darwin Guzman <guzmandp@gmail.com>
2021-02-21 16:56:42 -0300

    ORM mapping, database seeding

--- backend/src/main/java/com/guzmandp/dscatalog/entities/Category.java
+++ backend/src/main/java/com/guzmandp/dscatalog/entities/Category.java
@@ -2,12 +2,15 @@ package com.guzmandp.dscatalog.entities;
 
 import java.io.Serializable;
 import java.time.Instant;
+import java.util.HashSet;
+import java.util.Set;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToMany;
 import javax.persistence.PrePersist;
 import javax.persistence.PreUpdate;
 import javax.persistence.Table;
@@ -25,6 +28,8 @@ public class Category implements Serializable {
 	@Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
 	private Instant updateAt;
 	
+	@ManyToMany(mappedBy = "categories")
+	private Set<Product> products = new HashSet<>();
 	
 	public Category() {
 	}
@@ -68,6 +73,11 @@ public class Category implements Serializable {
 	public void preUpdate() {
 		updateAt = Instant.now();
 	}
+	
+	public Set<Product> getProducts() {
+		return products;
+	}
+
 	@Override
 	public int hashCode() {
 		final int prime = 31;
